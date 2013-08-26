1a06979f62b28689f2509c269e16a33c64893368
Brad Davis <bdavis@saintandreas.org>
2013-08-26 00:34:17 -0700

    Adding sensor fusion code to the Java utilities

---
+++ Bindings/Java/src/main/java/copied/com/jme3/math/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package is copied out of the JMonkeyengine math code, renamed to avoid
+ * potential classloader conflicts. The JME math code has greater functionality
+ * than either the LWGJL or javax.vecmath equivalents, such as including a
+ * Quaternion.mult(Vector3f) method
+ */
+package copied.com.jme3.math;
