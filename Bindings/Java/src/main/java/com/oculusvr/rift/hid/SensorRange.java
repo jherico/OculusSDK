513740fcdac4f78650ed1a7b0b4abc7b91530912
Brad Davis <bdavis@saintandreas.org>
2014-02-02 21:59:15 -0800

    Java bindings work

---
+++ Bindings/Java/src/main/java/com/oculusvr/rift/hid/SensorRange.java
@@ -0,0 +1,51 @@
+package com.oculusvr.rift.hid;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import com.codeminders.hidapi.HIDDevice;
+import com.oculusvr.input.hid.HidFeatureReport;
+
+/**
+ * Factor in the range conversion
+ * 
+ */
+public final class SensorRange extends HidFeatureReport {
+    public static final byte FEATURE_ID = 4;
+    public static final int FEATURE_SIZE = 8;
+
+    public static final float EARTH_GEES_TO_METERS_PER_SECOND = 9.81f;
+    public static final float MILLIGAUSS_TO_GAUSS = 0.001f;
+
+    public short commandId;
+    // Meters per second squared
+    public short accelerationScale;
+    // Radians
+    public short gyroScale;
+    // Gauss
+    public short magScale;
+
+    public SensorRange() {
+        super(FEATURE_ID, FEATURE_SIZE);
+    }
+
+    public SensorRange(HIDDevice device) throws IOException {
+        super(FEATURE_ID, FEATURE_SIZE, device);
+    }
+
+    @Override
+    public void parse(ByteBuffer buffer) {
+        commandId = buffer.getShort();
+        accelerationScale = buffer.get();
+        gyroScale = buffer.getShort();
+        magScale = buffer.getShort();
+    }
+
+    @Override
+    public void pack(ByteBuffer buffer) {
+        buffer.putShort(commandId);
+        buffer.put((byte) accelerationScale);
+        buffer.putShort(gyroScale);
+        buffer.putShort(magScale);
+    }
+}
---
+++ Bindings/Java/src/main/java/com/oculusvr/rift/tracker/TrackerMessage.java
@@ -0,0 +1,32 @@
+package com.oculusvr.rift.tracker;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+
+public class TrackerMessage {
+    public final int                 type;
+    public final int                 sampleCount;
+    public final int                 timestamp;
+    public final int                 lastCommandId;
+    public final int                 temperature;
+    public final List<TrackerSample> samples;
+    public final TrackerVector       mag;
+
+    public TrackerMessage(ByteBuffer buffer) {
+        type = buffer.get();
+        sampleCount = buffer.get() & 0xff;
+        timestamp = (buffer.getShort() & 0xffff);
+        lastCommandId = (buffer.getShort() & 0xffff);
+        temperature = (buffer.getShort() & 0xffff);
+        List<TrackerSample> samples = Lists.newArrayList();
+        for (int i = 0; i < 3; ++i) {
+            samples.add(new TrackerSample(buffer));
+        }
+        // this.samples = Lists.newArrayList();
+        this.samples = ImmutableList.copyOf(samples.subList(0, Math.min(3, sampleCount)));
+        mag = new TrackerVector(buffer, false);
+    }
+};
